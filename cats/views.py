df29699e5c60f35ff3157b9e9a88316a1bc246ce
Jesus Anaya <jesus.anaya.dev@gmail.com>
2015-09-08 14:54:59 -0500

    Testing if the Class view is the problem.

--- cats/views.py
+++ cats/views.py
@@ -1,21 +1,14 @@
-from django.views.generic import FormView, TemplateView
+from django.views.generic import CreateView, TemplateView
 from django.core.urlresolvers import reverse
 from .forms import SubscriberForm
-from .models import Subscriber
 
 
-class HomeView(FormView):
+class HomeView(CreateView):
     template_name = 'index.html'
     form_class = SubscriberForm
 
     def get_success_url(self):
         return reverse('thanks')
-
-    def form_valid(self, form):
-        email = form.cleaned_data.get('email')
-        if not Subscriber.objects.filter(email=email).exists():
-            form.save()
-        return super(HomeView, self).form_valid(form)
 
 
 class ThanksView(TemplateView):
